436524eeb4d865522c9e16d7848f89960f768506
Developer Kumar <amitkumar.developer1@gmail.com>
2020-09-18 18:56:53 +0530

    Uploading Project

---
+++ README.md
@@ -0,0 +1,165 @@
+# PARKING LOT PROJECT
+
+## DESCRIPTION
+
+---
+
+This is a parking lot project which aim is to reduce human intervention and to make it a automatic process.
+
+In this Project I have made a parking solution for parking owner/admin.
+
+The project can handle multi floor parking.
+
+The parking spaces are being stored in the **Dictionary** data type storing the slot_no as key and the car info as values
+
+The car info is again stored in the form of **Dictionay** data type having key and its value
+
+_For eg:_
+
+Parking Slot would be like = {'slot_no': Car_info OR Empty, 'slot_no': Car_info OR Empty, 'slot_no': Car_info OR Empty, ... So on}
+
+Car Info would be like = {'regno': 'KA-01-HH-1234', 'color': 'White', 'current_time': '08:31:06', 'email': 'xyz@gmail.com'}
+
+Empty/No Car would be like an empty dictionary = {}
+
+---
+
+## FEATURE
+
+It has some basic listed features:
+
+1. Create Parking lot
+
+2. Park a new car
+
+3. Leave a car
+
+4. Check Status of Parking Lot
+
+5. Search Car Registration No. by car Color
+
+6. Search car Slot No. by car color
+
+7. Search car Slot No. by car Registraion No.
+
+Extra Features:
+
+1. Making it multi floor parking having several floors
+
+2. Storing entry time of car and email address of car owner
+
+3. Checking car parking duration while checking the status of Parking Lot
+
+4. Getting an E-mail notification while leaving the parking lot about your time duration and other details.
+
+## EXPLAINING COMMANDS
+
+| Feature                             | Command                                                 | Eg                                                |
+| ----------------------------------- | ------------------------------------------------------- | ------------------------------------------------- |
+| Create Parking                      | create_parking_lot 'no of slot per floor' 'no of floor' | create_parking_lot 6 1                            |
+| Check Parking Lot Status            | status                                                  | status                                            |
+| Park a new car                      | park 'registration no' 'car color' 'email address'      | park KA-01-HH-1234 White amitkumar66966@gmail.com |
+| Leave a car                         | leave 'slot no'                                         | leave Floor1Slot4                                 |
+| Search registrtaion no. by color    | registration_numbers_for_cars_with_colour 'Car color'   | registration_numbers_for_cars_with_colour White   |
+| Search slot no. by color            | slot_numbers_for_cars_with_colour 'Car Color'           | slot_numbers_for_cars_with_colour White           |
+| Search slot no. by registration no. | slot_number_for_registration_number 'Registration No.'  | slot_number_for_registration_number KA-01-HH-1234 |
+
+## USAGE
+
+---
+
+Just go to through the main.py and run the code
+
+This will show you an option of selecting among 2 choice i.e. Running Terminal Commands by Press 1 || Running commands saved on file.txt by Press 2
+
+By choosing 1 you can type the commands and the resultant output would be there
+
+By choosing 2 the file.txt will run and prints the output of each command.
+
+### INPUT
+
+1
+
+create_parking_lot 6 1
+
+status
+
+park KA-01-HH-1234 White xyz@gmail.com
+
+park KA-01-HH-9999 White xyz@gmail.com
+
+park KA-01-BB-0001 Black xyz@gmail.com
+
+park KA-01-HH-7777 Red xyz@gmail.com
+
+park KA-01-HH-3141 Blue xyz@gmail.com
+
+park KA-01-HH-3141 Black xyz@gmail.com
+
+leave Floor1Slot4
+
+status
+
+park KA-01-P-333 White xyz@gmail.com
+
+park DL-12-AA-9999 White xyz@gmail.com
+
+registration_numbers_for_cars_with_colour White
+
+slot_numbers_for_cars_with_colour White
+
+slot_number_for_registration_number KA-01-HH-1234
+
+slot_number_for_registration_number MH-04-AY-1111
+
+### OUTPUT
+
+Created a parking lot with 6 slots
+
+Parking Slot is Empty
+
+Allocated slot number: floor1slot1
+
+Allocated slot number: floor1slot2
+
+Allocated slot number: floor1slot3
+
+Allocated slot number: floor1slot4
+
+Allocated slot number: floor1slot5
+
+Sorry, Same Car Already Park
+
+Check Your Email for Bill Invoice
+
+Slot number floor1slot4 is free.
+
+Duration Parked 0:00:00
+
+Slot No. Registration No Colour Entry_Time Duration Email
+
+floor1slot1 ka-01-hh-1234 white 17:58:46 0:00:03 xyz@gmail.com
+
+floor1slot2 ka-01-hh-9999 white 17:58:46 0:00:03 xyz@gmail.com
+
+floor1slot3 ka-01-bb-0001 black 17:58:46 0:00:03 xyz@gmail.com
+
+floor1slot5 ka-01-hh-3141 blue 17:58:46 0:00:03 xyz@gmail.com
+
+Allocated slot number: floor1slot4
+
+Allocated slot number: floor1slot6
+
+ka-01-hh-1234, ka-01-hh-9999, ka-01-p-333, dl-12-aa-9999
+
+floor1slot1, floor1slot2, floor1slot4, floor1slot6
+
+floor1slot1
+
+Not found
+
+---
+
+## AUTHOR
+
+AMIT KUMAR
---
+++ main.py
@@ -0,0 +1,245 @@
+class ParkingLot:
+    def __init__(self, total_space, no_floor=2):
+        self.parking = {}
+        total_space = int(total_space)
+        no_floor = int(no_floor)
+        i, j = 1, 1
+        while (j <= no_floor):
+            self.parking[('Floor' + str(j) + 'Slot' + str(i)).lower()] = {}
+            if i == total_space:
+                j += 1
+                i = 1
+            else:
+                i += 1
+        print("Created a parking lot with", len(self.parking), "slots")
+        self.total_car = 0
+        self.total_slot = len(self.parking)
+
+    def ParkCar(self, regno, color, email):
+        current_time = datetime.now().time()
+        for key, values in self.parking.items():
+            if values == {}:
+                values['regno'] = regno
+                values['color'] = color
+                values['current_time'] = current_time
+                values['email'] = email
+
+                print("Allocated slot number: ", key)
+                self.total_car += 1
+                return
+
+    def LeaveParkingSlot(self, slot):
+        if slot in self.parking.keys():
+            if self.parking[slot] != {}:
+                car_data = self.parking[slot]
+                duration = self.timeDuration(car_data['current_time'])
+                sendEmail(car_data['regno'],
+                          car_data['color'], duration, car_data['email'])
+                print("Slot number", slot, "is free.")
+                print("Duration Parked", duration)
+                self.total_car -= 1
+                self.parking[slot] = {}
+            else:
+                print("Slot number", slot, "is already free")
+        else:
+            print("Slot number", slot, "is not Present")
+
+    def timeDuration(self, start):
+        end = datetime.now().time()
+        t1 = timedelta(hours=start.hour,
+                       minutes=start.minute, seconds=start.second)
+        t2 = timedelta(
+            hours=end.hour, minutes=end.minute, seconds=end.second)
+        return t2 - t1
+
+    def PrintParkingLot(self):
+        print("Slot No. Registration No Colour Entry_Time Duration Email")
+        for key, values in self.parking.items():
+            if values != {}:
+                duration = self.timeDuration(values['current_time'])
+                curr_time = values['current_time'].strftime("%H:%M:%S")
+                print(key, values['regno'], values['color'],
+                      curr_time, duration, values['email'])
+
+    def FetchRegNoByColor(self, color):
+        notFound = True
+        for key, values in self.parking.items():
+            if self.parking[key] != {}:
+                if self.parking[key]['color'] == color:
+                    if notFound:
+                        print(values['regno'], end="")
+                        notFound = False
+                    else:
+                        print(",", values['regno'])
+        if notFound:
+            print("Not found")
+
+    def FetchSlotByColor(self, color):
+        notFound = True
+        for key in self.parking:
+            if self.parking[key] != {}:
+                if self.parking[key]['color'] == color:
+                    if notFound:
+                        print(key, end="")
+                        notFound = False
+                    else:
+                        print(",", key)
+        if notFound:
+            print("Not found")
+
+    def FetchSlotByRegNo(self, regno):
+        for key, values in self.parking.items():
+            if values != {}:
+                if values['regno'] == regno:
+                    print(key)
+                    return
+        print("Not found")
+
+    def isNotFull(self):
+        return 1 if self.total_car != self.total_slot else 0
+
+    def isEmpty(self):
+        return 1 if self.total_car == 0 else 0
+
+    def carNotExist(self, regno):
+        for key in self.parking:
+            if self.parking[str(key)] != {}:
+                if self.parking[str(key)]['regno'] == regno:
+                    return 0
+        return 1
+
+
+def sendEmail(regno, color, duration, receiver):
+    sender = 'anonymous.people.one@gmail.com'
+    password = 'slxfyyaaemsukkdv'
+    message = MIMEMultipart("alternative")
+    message["Subject"] = "Car Parking Checkout"
+    message["From"] = 'Car Parking'
+    message["To"] = receiver
+    regno = regno.upper()
+    color = color.upper()
+    # write the plain text part
+    text = """\
+    Hi User,
+    Check out the details of your car parking:
+    Registration Number :{}
+    Color :{}
+    Duration :{}
+
+    Feel free to let us know for any query!
+    We Hope To See You Soon
+    Thank You
+    """.format(regno, color, duration)
+    # write the HTML part
+    html = """\
+    <html>
+    <body>
+        <h3 style='color:#4472C4;'>Hi User,</h3>
+        <p>Check out the details of your car parking:&nbsp;&nbsp;&nbsp;</p>
+        <p><strong>Registration Number : </strong>{}</p>
+        <p><strong>Color : </strong>{}</p>
+        <p><strong>Duration : </strong>{}</p>
+        <p><strong>&nbsp;</strong></p>
+        <p style='color:green;'>Feel free to let us know for any query!</p>
+        <p style='color:green;'>We Hope To See You Soon Again</p>
+        <p><em style='color:red;'><b>Thank You</b></em></p>
+    </body>
+    </html>
+    """.format(regno, color, duration)
+    part1 = MIMEText(text, "plain")
+    part2 = MIMEText(html, "html")
+    message.attach(part1)
+    message.attach(part2)
+
+    try:
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.ehlo()
+        server.login(sender, password)
+        server.sendmail(sender, receiver, message.as_string())
+        server.close()
+        print('Check Your Email for Bill Invoice')
+    except Exception:
+        print('Error Occured while sending email')
+
+
+def isEmailValid(email):
+    regex = "^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$"
+    if(re.search(regex, email)):
+        return 1
+    else:
+        return 0
+
+
+if __name__ == "__main__":
+    from datetime import datetime, timedelta
+    import re
+    import smtplib
+    from email.mime.text import MIMEText
+    from email.mime.multipart import MIMEMultipart
+
+    a = int(input("1 for Terminal Input and 2 for Executing File"))
+    if a == 2:
+        input_file = open(
+            "D:/Technical Stuffs/Attainu Course/Parking-lot/file.txt", "r")
+    while(True):
+        if (a == 1):
+            take = input().lower().split(" ")
+        elif (a == 2):
+            all_input_lines = input_file.readline()
+            all_input_lines = all_input_lines.lower().replace('\n', '')
+            take = all_input_lines.split(" ")
+            if take[0] == '':
+                break
+        try:
+            if take[0] == 'create_parking_lot':
+                NewParking = ParkingLot(take[1], take[2])
+
+            elif take[0] == 'park':
+                if NewParking.isNotFull():
+                    if NewParking.carNotExist(take[1]):
+                        if isEmailValid(take[3]):
+                            NewParking.ParkCar(take[1], take[2], take[3])
+                        else:
+                            print("Email address is not valid")
+                    else:
+                        print("Sorry, Same Car Already Park")
+                else:
+                    print("Sorry, parking lot is full")
+
+            elif take[0] == 'leave':
+                if(NewParking.isEmpty()):
+                    print("Parking Slot is Empty")
+                else:
+                    NewParking.LeaveParkingSlot(take[1])
+
+            elif take[0] == 'status':
+                if(NewParking.isEmpty()):
+                    print("Parking Slot is Empty")
+                else:
+                    NewParking.PrintParkingLot()
+
+            elif take[0] == 'registration_numbers_for_cars_with_colour':
+                if(NewParking.isEmpty()):
+                    print("Parking Slot is Empty")
+                else:
+                    NewParking.FetchRegNoByColor(take[1])
+
+            elif take[0] == 'slot_numbers_for_cars_with_colour':
+                if(NewParking.isEmpty()):
+                    print("Parking Slot is Empty")
+                else:
+                    NewParking.FetchSlotByColor(take[1])
+
+            elif take[0] == 'slot_number_for_registration_number':
+                if(NewParking.isEmpty()):
+                    print("Parking Slot is Empty")
+                else:
+                    NewParking.FetchSlotByRegNo(take[1])
+
+            elif take[0] == 'exit':
+                break
+            print()
+        except Exception:
+            print("Wrong input || Error Occured")
+    if a == 2:
+        input_file.close()
